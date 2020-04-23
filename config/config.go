30e8e4e2f7c93b59feaf5ef26548858b24eb5168
Arash Hatami <hatamiarash7@gmail.com>
2020-04-23 23:57:50 +0430

    Create config.go

---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"io"
+	"io/ioutil"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+// Sensor is the output structure for ESP8266
+type Sensor struct {
+    Temperature string `json:"temperature"`
+    Humidity string `json:"humidity"`
+}
+
+// Config is structure of config file
+type Config struct {
+    Clients []Client `yaml:"clients"`
+}
+
+// Client is array of clients in config file
+type Client struct{
+	Name string `yaml:"name"`
+	IP string `yaml:"ip"`
+}
+
+// Load reads YAML from reader and unmarshals in Config
+func Load(r io.Reader) (*Config, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Config{}
+	err = yaml.Unmarshal(b, c)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
