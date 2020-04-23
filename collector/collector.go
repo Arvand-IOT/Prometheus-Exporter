4c77f6a82905310ae6058607de328418cfd60c2d
Arash Hatami <hatamiarash7@gmail.com>
2020-04-24 02:49:49 +0430

    collect data from all devices

--- collector/collector.go
+++ collector/collector.go
@@ -1,38 +1,72 @@
 package collector
 
 import (
-	// "encoding/binary"
-    // "fmt"
-    // "math"
-    // "os/exec"
-    // "strings"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strconv"
 
+	"arvand-exporter/config"
 	"github.com/prometheus/client_golang/prometheus"
 	// log "github.com/sirupsen/logrus"
 )
 
+var (
+	clients []config.Client
+)
+
 // SensorCollector ...
 type SensorCollector struct {
-    cmdMetric *prometheus.Desc
+	temperatureMetric *prometheus.Desc
+	humidityMetric    *prometheus.Desc
 }
 
 // NewCollector ...
-func NewCollector() *SensorCollector {
-    return &SensorCollector{
-        cmdMetric: prometheus.NewDesc("cmd_result",
-            "Shows the cmd result",
-            []string{"name"}, nil,
-        ),
-    }
+func NewCollector(c []config.Client) *SensorCollector {
+	clients = c
+
+	return &SensorCollector{
+		temperatureMetric: prometheus.NewDesc("temperature",
+			"temperature value in celsius",
+			[]string{"name"}, nil,
+		),
+		humidityMetric: prometheus.NewDesc("humidity",
+			"humidity value from 100",
+			[]string{"name"}, nil,
+		),
+	}
 }
 
 // Describe ...
 func (collector *SensorCollector) Describe(ch chan<- *prometheus.Desc) {
-    ch <- collector.cmdMetric
+	ch <- collector.temperatureMetric
+	ch <- collector.humidityMetric
 }
 
 // Collect ...
 func (collector *SensorCollector) Collect(ch chan<- prometheus.Metric) {
-    ch <- prometheus.MustNewConstMetric(collector.cmdMetric, prometheus.GaugeValue, 1, "rack")
-    ch <- prometheus.MustNewConstMetric(collector.cmdMetric, prometheus.GaugeValue, 2, "rack2")
+	for _, client := range clients {
+		url := "http://" + client.IP + "/data"
+
+		res, err := http.Get(url)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		var data *config.Sensor
+		json.Unmarshal(body, &data)
+
+		t, _ := strconv.ParseFloat(data.Temperature, 64)
+		h, _ := strconv.ParseFloat(data.Humidity, 64)
+
+		ch <- prometheus.MustNewConstMetric(collector.temperatureMetric, prometheus.GaugeValue, t, client.Name)
+		ch <- prometheus.MustNewConstMetric(collector.humidityMetric, prometheus.GaugeValue, h, client.Name)
+	}
 }
