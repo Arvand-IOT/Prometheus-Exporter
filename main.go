f92dc03f7fa8a464038b25750c7f9fe1ed351904
Arash Hatami <hatamiarash7@gmail.com>
2020-04-24 02:52:11 +0430

    remove old codes

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"encoding/json"
-	"strconv"
 
 	"arvand-exporter/collector"
 	"arvand-exporter/config"
@@ -26,20 +24,6 @@ var (
 	logFormat   = flag.String("log-format", "json", "logformat text or json (default json)")
 
 	cfg *config.Config
-)
-
-var (
-	// TemperatureGauge is the temperature of sensor in celsius
-	TemperatureGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "temperature",
-		Help: "Current temperature of the sensor.",
-	})
-
-	// HumidityGauge is the humidity of sensor ( 0 - 100 )
-	HumidityGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "humidity",
-		Help: "Current humidity of the sensor.",
-	})
 )
 
 func main() {
@@ -94,17 +78,8 @@ func loadConfigFromFile() (*config.Config, error) {
 }
 
 func startServer() {
-	// collectData()
-
-	// handler, err := createMetricsHandler()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// http.Handle(*metricsPath, handler)
-
 	cmd := collector.NewCollector(cfg.Clients)
-    prometheus.MustRegister(cmd)
+	prometheus.MustRegister(cmd)
 
 	http.Handle("/metrics", promhttp.Handler())
 
@@ -121,73 +96,3 @@ func startServer() {
 	log.Info("Listening on ", *port)
 	log.Fatal(http.ListenAndServe(*port, nil))
 }
-
-func createMetricsHandler() (http.Handler, error) {
-	// opts := collectorOptions()
-	// nc, err := collector.NewCollector(cfg, opts...)
-	// nc, err := collector.NewCollector()
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	registry := prometheus.NewRegistry()
-	registry.MustRegister(TemperatureGauge)
-	registry.MustRegister(HumidityGauge)
-
-	// err = registry.Register(nc)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	return promhttp.HandlerFor(registry,
-		promhttp.HandlerOpts{
-			ErrorLog:      log.New(),
-			ErrorHandling: promhttp.ContinueOnError,
-		}), nil
-}
-
-func collectData() {
-	url := "http://192.168.1.29/data"
-
-	res, err := http.Get(url)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	var data *config.Sensor
-	json.Unmarshal(body, &data)
-
-	t, _ := strconv.ParseFloat(data.Temperature, 64)
-	h, _ := strconv.ParseFloat(data.Humidity, 64)
-
-	TemperatureGauge.Set(t)
-	HumidityGauge.Set(h)
-}
-
-// func collectorOptions() []collector.Option {
-// 	opts := []collector.Option{}
-
-// 	opts = append(opts, collector.WithTemp())
-
-// 	return opts
-// }
-
-// func main() {
-// 	var conf Conf
-// 	var data Sensor
-//     conf.getConf()
-
-// 	for _, client := range conf.Clients {
-// 		data = get(client.IP)
-// 		fmt.Println(client.IP , " : " , data.Temperature)
-// 	}
-
-// 	os.Exit(1)
-// }
