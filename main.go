3fc21608aa8b1fc178f8da5ed6a5c8ada2650567
Arash Hatami <hatamiarash7@gmail.com>
2020-04-23 23:58:02 +0430

    add prometheus collector

--- main.go
+++ main.go
@@ -1,39 +1,147 @@
 package main
 
-import "os"
-import "fmt"
-import "net/http"
-import "io/ioutil"
-import "encoding/json"
+import (
+	"os"
+	"fmt"
+	"flag"
+	"net/http"
+	"io/ioutil"
+	"bytes"
+	// "encoding/json"
+	
+	"arvand-exporter/config"
+	// "arvand-exporter/collector"
 
-// Sensor is the output structure for ESP8266
-type Sensor struct {
-    Temperature string `json:"temperature"`
-    Humidity string `json:"humidity"`
-}
+	log "github.com/sirupsen/logrus"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
-func get() {
-    url := "http://192.168.1.29/data"
+var (
+	configFile  = flag.String("config-file", "", "config file to load")
+	metricsPath = flag.String("path", "/metrics", "path to answer requests on")
+	port        = flag.String("port", ":9437", "port number to listen on")
+	logLevel    = flag.String("log-level", "info", "log level")
+	logFormat   = flag.String("log-format", "json", "logformat text or json (default json)")
 
-    res, err := http.Get(url)
+	cfg *config.Config
+)
 
-    if err != nil {
-        panic(err.Error())
-    }
+var (
+	// TemperatureGauge is the temperature of sensor in celsius
+	TemperatureGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "temperature",
+		Help: "Current temperature of the sensor.",
+	})
 
-    body, err := ioutil.ReadAll(res.Body)
-
-    if err != nil {
-        panic(err.Error())
-    }
-
-	var data Sensor
-	json.Unmarshal(body, &data)
-	fmt.Println(data.Temperature)
-	fmt.Println(data.Humidity)
-    os.Exit(0)
-}
+	// HumidityGauge is the humidity of sensor ( 0 - 100 )
+	HumidityGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "humidity",
+		Help: "Current humidity of the sensor.",
+	})
+)
 
 func main() {
-    get()
+	flag.Parse()
+
+	configureLog()
+
+	log.Info("Welcome to Arvand Prometheus Exporter")
+
+	log.Info("Version : 1.0")
+
+	c, err := loadConfig()
+	if err != nil {
+		log.Errorf("Could not load config : %v", err)
+		os.Exit(3)
+	}
+	cfg = c
+
+	startServer()
 }
+
+func configureLog() {
+	ll, err := log.ParseLevel(*logLevel)
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetLevel(ll)
+
+	if *logFormat == "text" {
+		log.SetFormatter(&log.TextFormatter{})
+	} else {
+		log.SetFormatter(&log.JSONFormatter{})
+	}
+}
+
+func loadConfig() (*config.Config, error) {
+	if *configFile != "" {
+		return loadConfigFromFile()
+	}
+
+	return nil, fmt.Errorf("missing config file")
+}
+
+func loadConfigFromFile() (*config.Config, error) {
+	b, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return config.Load(bytes.NewReader(b))
+}
+
+func startServer() {
+	TemperatureGauge.Set(65.3)
+	HumidityGauge.Set(64.3)
+
+	handler, err := createMetricsHandler()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle(*metricsPath, handler)
+
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(`<html>
+			<head><title>Mikrotik Exporter</title></head>
+			<body>
+			<h1>Mikrotik Exporter</h1>
+			<p><a href="` + *metricsPath + `">Metrics</a></p>
+			</body>
+			</html>`))
+	})
+
+	log.Info("Listening on ", *port)
+	log.Fatal(http.ListenAndServe(*port, nil))
+}
+
+func createMetricsHandler() (http.Handler, error) {
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(TemperatureGauge)
+	registry.MustRegister(HumidityGauge)
+
+	return promhttp.HandlerFor(registry,
+		promhttp.HandlerOpts{
+			ErrorLog:      log.New(),
+			ErrorHandling: promhttp.ContinueOnError,
+		}), nil
+}
+
+// func main() {
+// 	var conf Conf
+// 	var data Sensor
+//     conf.getConf()
+
+// 	for _, client := range conf.Clients {
+// 		data = get(client.IP)
+// 		fmt.Println(client.IP , " : " , data.Temperature)
+// 	}
+
+// 	os.Exit(1)
+// }
