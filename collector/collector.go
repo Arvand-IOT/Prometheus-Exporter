65b118827ccdebf87892d173277088e8c6406d29
Arash Hatami <hatamiarash7@gmail.com>
2020-04-24 15:53:39 +0430

    update collect process
    - measure collect time
    - add http timeout

--- collector/collector.go
+++ collector/collector.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"time"
 
 	"arvand-exporter/config"
 	"github.com/prometheus/client_golang/prometheus"
@@ -45,34 +46,50 @@ func (collector *SensorCollector) Describe(ch chan<- *prometheus.Desc) {
 
 // Collect data for metrics
 func (collector *SensorCollector) Collect(ch chan<- prometheus.Metric) {
+	start := time.Now()
+
 	for _, client := range clients {
 		url := "http://" + client.IP + "/data"
 
-		res, err := http.Get(url)
-
-		if err != nil {
-			panic(err.Error())
+		httpClient := http.Client{
+			Timeout: 5 * time.Second,
 		}
+		res, err := httpClient.Get(url)
 
-		body, err := ioutil.ReadAll(res.Body)
+		var t float64 = 0.0
+		var h float64 = 0.0
 
 		if err != nil {
 			log.WithFields(log.Fields{
 				"IP": client.IP,
-			}).Warn("Error fetching data !")
+			}).Warn("Client not reachable !")
+		} else {
+
+			body, err := ioutil.ReadAll(res.Body)
+
+			if err != nil {
+				log.WithFields(log.Fields{
+					"IP": client.IP,
+				}).Warn("Error fetching data !")
+			} else {
+
+				log.WithFields(log.Fields{
+					"IP": client.IP,
+				}).Info("Done !")
+
+				var data *config.Sensor
+				json.Unmarshal(body, &data)
+
+				t, _ = strconv.ParseFloat(data.Temperature, 64)
+				h, _ = strconv.ParseFloat(data.Humidity, 64)
+
+				ch <- prometheus.MustNewConstMetric(collector.temperatureMetric, prometheus.GaugeValue, t, client.Name)
+				ch <- prometheus.MustNewConstMetric(collector.humidityMetric, prometheus.GaugeValue, h, client.Name)
+			}
+
 		}
-
-		log.WithFields(log.Fields{
-			"IP": client.IP,
-		}).Info("Done !")
-
-		var data *config.Sensor
-		json.Unmarshal(body, &data)
-
-		t, _ := strconv.ParseFloat(data.Temperature, 64)
-		h, _ := strconv.ParseFloat(data.Humidity, 64)
-
-		ch <- prometheus.MustNewConstMetric(collector.temperatureMetric, prometheus.GaugeValue, t, client.Name)
-		ch <- prometheus.MustNewConstMetric(collector.humidityMetric, prometheus.GaugeValue, h, client.Name)
 	}
+
+	elapsed := time.Since(start)
+	log.Info("All data collected in ", elapsed)
 }
